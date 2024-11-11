d9b0eb4863ac6f2fc43c00c96d5f8811d9b50c71
JordanTrindade <jordandimst@gmail.com>
2024-11-11 17:15:32 -0300

    Create Endereco.java

---
+++ src/main/java/com/ecommerce/model/Endereco.java
@@ -0,0 +1,97 @@
+package com.ecommerce.model;
+
+import com.ecommerce.enums.TipoCliente;
+import jakarta.persistence.*;
+
+@Entity
+public class Endereco {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    public Endereco(String logadouro, String numero, String complemento, String bairro, String cep, Cliente cliente, Cidade cidade) {
+        this.logadouro = logadouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.cep = cep;
+        this.cliente = cliente;
+        this.cidade = cidade;
+    }
+
+    private String logadouro;
+    private String numero;
+    private String complemento;
+    private String bairro;
+    private String cep;
+
+    @ManyToOne
+    private Cliente cliente;
+
+    @ManyToOne
+    private Cidade cidade;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getLogadouro() {
+        return logadouro;
+    }
+
+    public void setLogadouro(String logadouro) {
+        this.logadouro = logadouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public Cidade getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(Cidade cidade) {
+        this.cidade = cidade;
+    }
+}
