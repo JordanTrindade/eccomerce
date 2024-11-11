982b48f7b8df4e2912d49a5cd688bc71ae2d0cf7
JordanTrindade <jordandimst@gmail.com>
2024-11-11 17:15:21 -0300

    Update TipoCliente.java

--- src/main/java/com/ecommerce/enums/TipoCliente.java
+++ src/main/java/com/ecommerce/enums/TipoCliente.java
@@ -1,6 +1,36 @@
 package com.ecommerce.enums;
 
 public enum TipoCliente {
-        PESSOAFISICA,
-        PESSOAJURICICA
+        PESSOAFISICA(1,"PESSOA FISICA"),
+        PESSOAJURICICA(2,"PESSOA JURIDICA");
+
+        private final Integer id;
+        private final String descricao;
+
+        TipoCliente(Integer id, String descricao){
+                this.id = id;
+                this.descricao = descricao;
+        }
+
+        public Integer getId() {
+                return id;
+        }
+
+        public String getDescricao() {
+                return descricao;
+        }
+
+        public static TipoCliente IntegertoTipoCliente(Integer id) {
+
+                if (id == null) {
+                        return null;
+                }
+
+                for (TipoCliente tipo : TipoCliente.values()) {
+                        if (id.equals(tipo.getId())) {
+                                return tipo;
+                        }
+                }
+                throw new IllegalArgumentException("Id inválido " + id);
+        }
 }
