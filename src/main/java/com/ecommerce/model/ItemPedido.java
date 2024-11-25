ad420df5db17acc26cf7a8e2f35fc42d79b2adbb
JordanTrindade <jordandimst@gmail.com>
2024-11-25 13:11:49 -0300

    Create ItemPedido.java

---
+++ src/main/java/com/ecommerce/model/ItemPedido.java
@@ -0,0 +1,9 @@
+package com.ecommerce.model;
+
+public class ItemPedido {
+
+    private ItemPedidoPK id = new ItemPedidoPK();
+    private Double desconto;
+    private Integer quantidade;
+    private Double preço;
+}
