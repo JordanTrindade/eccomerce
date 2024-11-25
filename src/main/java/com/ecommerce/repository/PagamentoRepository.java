4fed5dcdd30c457d426546a72fcdc65090226843
JordanTrindade <jordandimst@gmail.com>
2024-11-25 13:12:17 -0300

    Create PagamentoRepository.java

---
+++ src/main/java/com/ecommerce/repository/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.ecommerce.repository;
+
+import com.ecommerce.model.Pagamento;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
+}
